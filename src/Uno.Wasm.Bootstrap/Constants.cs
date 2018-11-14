699cd332d8124dd02b63a7de0b79e38316b84f02
Jérôme Laban <jerome.laban@nventive.com>
2018-11-14 11:32:43 -0500

    wip

--- src/Uno.Wasm.Bootstrap/Constants.cs
+++ src/Uno.Wasm.Bootstrap/Constants.cs
@@ -6,7 +6,7 @@ namespace Uno.Wasm.Bootstrap
 {
 	internal class Constants
 	{
-		public const string DefaultSdkUrl = "https://jenkins.mono-project.com/job/test-mono-mainline-wasm/1331/label=ubuntu-1804-amd64/Azure/processDownloadRequest/1331/ubuntu-1804-amd64/sdks/wasm/mono-wasm-a682a3b2b63.zip";
-		public const string DefaultAotUrl = "https://jenkins.mono-project.com/job/test-mono-mainline-wasm/1331/label=ubuntu-1804-amd64/Azure/processDownloadRequest/1331/ubuntu-1804-amd64/wasm-release-Linux-a682a3b2b635194a22e7e4bb93088e85c922fb95.zip";
+		public const string DefaultSdkUrl = "https://jenkins.mono-project.com/job/test-mono-mainline-wasm/1339/label=ubuntu-1804-amd64/Azure/processDownloadRequest/1339/ubuntu-1804-amd64/sdks/wasm/mono-wasm-c0da2a51265.zip";
+		public const string DefaultAotUrl = "https://jenkins.mono-project.com/job/test-mono-mainline-wasm/1339/label=ubuntu-1804-amd64/Azure/processDownloadRequest/1339/ubuntu-1804-amd64/wasm-release-Linux-c0da2a5126514ab4136e537e2dea59670df5f0e2.zip";
 	}
 }
