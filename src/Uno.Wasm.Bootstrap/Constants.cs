c4ceb918203e7919bfb6ae932c6f2c3ba118d9f4
Jerome Laban <jerome.laban@nventive.com>
2020-05-05 08:19:03 -0400

    feat: Update to mono, enable IDBFS by default for interpreter builds, emscripten 1.39.14
    Uses mono build from https://github.com/unoplatform/Uno.Wasm.MonoRuntime/commit/c76c8a66029534eaf81775ad50cd98fe9e162431

--- src/Uno.Wasm.Bootstrap/Constants.cs
+++ src/Uno.Wasm.Bootstrap/Constants.cs
@@ -7,8 +7,8 @@ namespace Uno.Wasm.Bootstrap
 	internal class Constants
 	{
 		// NOTE: The SDK version may be overriden by an installation of the https://www.nuget.org/packages/Uno.Wasm.MonoRuntime nuget package
-		public const string DefaultSdkUrl = @"https://unowasmbootstrap.blob.core.windows.net/runtime/mono-wasm-88cae11c6b7.zip";
-		public const string DefaultAotSDKUrl = @"https://unowasmbootstrap.blob.core.windows.net/runtime/wasm-release-Linux-88cae11c6b7a212b1ddcc3aa4dc8f83d06a702ca.zip";
+		public const string DefaultSdkUrl = @"https://unowasmbootstrap.blob.core.windows.net/runtime/mono-wasm-fdc474bc46f.zip";
+		public const string DefaultAotSDKUrl = @"https://unowasmbootstrap.blob.core.windows.net/runtime/wasm-release-Linux-fdc474bc46f88bb01b0da22aaff0fbd9b88d421f.zip";
 
 		/// <summary>
 		/// Min version of the emscripten SDK. Must be aligned with mono's SDK build in <see cref="DefaultAotSDKUrl"/>.
@@ -17,6 +17,6 @@ namespace Uno.Wasm.Bootstrap
 		/// The emscripten version use by mono can be found here:
 		/// https://github.com/mono/mono/blob/master/sdks/builds/wasm.mk#L4
 		/// </remarks>
-		public static Version EmscriptenMinVersion { get; } = new Version("1.39.11");
+		public static Version EmscriptenMinVersion { get; } = new Version("1.39.14");
 	}
 }
